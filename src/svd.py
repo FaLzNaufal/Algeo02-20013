428a0f53e96d6830e72c380a11dcf2b379aa4a00
Cryptofal <88304292+FaLzNaufal@users.noreply.github.com>
2021-11-11 01:30:37 +0700

    Update svd.py

--- src/svd.py
+++ src/svd.py
@@ -6,51 +6,15 @@ def readImage():
     imgMatrix = np.array(img.convert('L'))   
     return imgMatrix
 
-def transpose(matrix):
+def transpose(matrix): #tidak dipakai, jadinya pakai fungsi bawaan numpy
     return np.array([[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))])
 
-def multiply(matrix1, matrix2):
+def multiply(matrix1, matrix2): #tidak dipakai, jadinya pakai fungsi bawaan numpy
     return np.array([[sum([matrix1[i][k]*matrix2[k][j] for k in range(len(matrix1[0]))]) for j in range(len(matrix2[0]))] for i in range(len(matrix1))])
 
-# def getEchelon(matrix):
-#     echelon = matrix[:]
-#     lastRow = 0
-#     for i in range(len(echelon[0])):
-#         for j in range(lastRow, len(echelon)):
-#             if echelon[j][i] != 0:
-#                 echelon[lastRow], echelon[j] = echelon[j], echelon[lastRow]
-#                 lastRow =  lastRow + 1
-#         if lastRow > len(echelon):
-#             break
-#     lastRow = 0
-#     for i in range(len(echelon)):
-#         for j in range(len(echelon[0])):
-#             if echelon[i][j] == 1:
-#                 break
-#             if echelon[i][j] != 0:
-#                 ratio = echelon[i][j]
-#                 print(echelon[i][j],ratio)
-#                 for k in range(j, len(echelon[i])):
-#                     echelon[i][k] /= ratio
-#                 break
-
-
-#     return echelon
-
-
-def getSolution(matrix):#matrixnya yg belum dikonjugasiin
-    conjugated = np.array([[matrix[i][j] for j in range(len(matrix[0]))] for i in range(len(matrix))], dtype='int16')
-    for r in conjugated:
-        r.append(0)
-    for r in conjugated:
-        print(r)
-    print()
-    solution = conjugated
-    return solution
-
 def svd(matrix):
-    transposed = transpose(matrix)
-    ata = multiply(transposed, matrix)
+    transposed = np.transpose(matrix)
+    ata = np.matmul(transposed, matrix)
     #fungsi eigen harusnya disini
     imgFromArray = Image.fromarray(matrix)
     imgFromArray.show()

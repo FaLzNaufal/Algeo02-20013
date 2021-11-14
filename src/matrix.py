b2a730414e7abdb6cdf208ba2e96eb5a7b684686
Moses-ui <73773603+Moses-ui@users.noreply.github.com>
2021-11-14 09:46:11 +0700

    meng-lelah

--- src/matrix.py
+++ src/matrix.py
@@ -1,191 +1,59 @@
 import numpy as np
-from scipy.linalg import hessenberg
-import sympy as sy
+from sympy import *
 
-# Fungsi yang me-return matrix kosong berukuran nRow dan nCol
-def createMatrix(nRow,nCol) :
-    matrix = [[0 for col in range(nCol)] for row in range(nRow)]
-    return matrix
+def eig(mat) :
 
-# Prosedur untuk menampilkan matrix
-def displayMatrix(matrix) :
-    nRow = len(matrix)
-    nCol = len(matrix[0])
-    for row in range(nRow) :
-        for col in range(nCol) :
-            print(matrix[row][col], end=" ")
-            if col == nRow - 1 :
-                print("")
+    x = Symbol('x') # Anggap aja lambda
+    I = (eye(shape(mat)[0]) * x) - mat
+    I = I.det()
 
-# Fungsi untuk menghitung determinan matrix
-# Prekondisi : harus matrix persegi
-def determinanMatrix(matrix) :
-    matrixLength = len(matrix)
-    if (matrixLength == 2) :
-        return (matrix[0][0] * matrix[1][1]) - (matrix[0][1] * matrix[1][0])
-    else :
-        determinan = 0
-        for col in range(matrixLength) :
-            determinan += ((-1) ** col) * matrix[0][col] * determinanMatrix(partMatrix(matrix, 0, col))
-        return determinan
+    result = list(solveset(I,x)) # berisi eigen value, tapi masih dalam bentuk Sympy
 
-# Fungsi yang me-return matrix jika dipartisi di baris nthRow dan kolom nthCol
-def partMatrix(matrix, nthRow, nthCol) :
-    nRow = nCol = len(matrix)
-    newMatrix = [[0 for row in range(nRow - 1)] for col in range(nCol - 1)]
-    idxRow = idxCol = 0
-    for row in range(nRow) :
-        for col in range(nCol) :
-            if row != nthRow and col != nthCol :
-                newMatrix[idxRow][idxCol] = matrix[row][col]
-                if idxCol == len(newMatrix) - 1 :
-                    idxRow += 1
-                    idxCol = 0
-                else :
-                    idxCol += 1
-    return newMatrix
+    arr_of_res = []
+    for item in result :
+        matCopy = np.copy(mat)
+        identity = eye(shape(mat)[0]) * item
+        identity -= matCopy
 
-# Fungsi yg me-return transpose matrix
-def transposeMatrix(matrix) :
-    nRow = len(matrix)
-    nCol = len(matrix[0])
-    transMat = createMatrix(nCol, nRow)
-    for row in range(nRow) :
-        for col in range(nCol) :
-            transMat[col][row] = matrix[row][col]
-    return transMat
+        gauss, pivot = identity.rref() # Masih harus dicari nilai x1, x2, dan x3
 
-# Return adjoin matriks
-def adjMatrix(matrix) :
-    length = len(matrix)
-    temp = createMatrix(length, length)
-    for row in range(length) :
-        for col in range(length) :
-            temp[row][col] =  ((-1) ** (col + row)) * determinanMatrix(partMatrix(matrix,row,col))
-    temp = transposeMatrix(temp)
-    return temp
+        gauss = np.array(gauss) # biar bisa dapat len(gauss[0])
 
-# Return invers matriks
-def inverseMatrix(matriks) :
-    length = len(matriks)
-    det = determinanMatrix(matriks)
-    if det : # det != 0
-        temp = adjMatrix(matriks)
-        for row in range(length) :
-            for col in range(length) :
-                temp[row][col] /= det
-    return temp
+        n = len(gauss) - len(pivot)
+        res = np.zeros((len(gauss), n))
+        for col in range(len(gauss)) :
+            if (col not in pivot) :
+                i = 0
+                for row in range(len(gauss)) :
+                    res[row][i] = -1 * gauss[row][col]
+                res[col][i] = 1
+                i += 1
 
-# Return nilai - nilai eigen
-def eigenValues(matrix, nIteration = 50000) :
-    temp = np.copy(matrix) 
-    n = temp.shape[0] 
-    if n == 2 :
-        temp = temp.tolist()
-        linier = temp[0][0] + temp[1][1]
-        linier *= -1
-        const = temp[0][0] * temp[1][1] - temp[0][1] * temp[1][0]
-        coeff = [1, linier, const]
-        result = np.roots(coeff)
-    else :
-        temp = toHessenberg(temp)
-        for k in range(nIteration): 
-            s = temp.item(n-1, n-1) 
-            smult = s * np.eye(n)
-            Q, R = np.linalg.qr(np.subtract(temp, smult))
-            temp = np.add(R @ Q, smult)
-
-        result = [0 for i in range(n)]
-        for j in range(n) :
-            result[j] = round(temp.item(j,j),5)
-        result = sorted(result, reverse=False)
-
-    return result # result berisi eigenvalue terurut membesar, biar mirip np.linalg.eig
-
-# return eselon baris tereduksi
-def gaussJordan(matriks) :
-    matriks, pivot = sy.Matrix(matriks).rref()
-    matriks = np.array(matriks.tolist())
-    return matriks, pivot
-
-def eigenVector(matriks, lamda) :
-    temp = np.copy(matriks)
-    I = np.eye(len(matriks)) * lamda
-    temp = np.subtract(temp, I)
-
-    mat, pivot = gaussJordan(temp)
-    mat = mat.tolist()
-    n = len(mat) - len(pivot)
-    res = createMatrix(len(matriks),n)
-    for col in range(len(mat[0])) :
-        if (col not in pivot) :
-            i = 0
-            for row in range(len(mat)) :
-                res[row][i] = -1 * mat[row][col]
-            res[col][i] = 1
-            i += 1
-
-    # n = len(mat) - len(pivot)
-
-    # firstZero = 0
-    # for row in range(len(res)) :
-    #     if res[row][0] == 0 :
-    #         firstZero = row
-    #         break
-
-    # nRow = len(res)
-    # firstZero *= -1
-    # res = np.pad(res, ((0,0),(0,n-1)), mode='constant', constant_values=0)
-    # I = np.eye(nRow, n, k=firstZero)
-    # res = res + I
-    # res = res.tolist()
-
-    return res
-
-def eigen(matrix) :
-    arr_of_lambda = eigenValues(matrix)
-    arr_of_eigenvector = []
-    for lamda in arr_of_lambda :
-        lamda = round(lamda)
-        vector = eigenVector(matrix, lamda)
         sum = 0
-        for item in vector :
+        for item in res :
             sum += item[0] * item[0]
         sum = sum ** 0.5
-        for i in range(len(vector)) :
-            vector[i] = (vector[i][0] / sum) * (-1)
-        arr_of_eigenvector.append(vector)
-    arr_of_eigenvector = transposeMatrix(arr_of_eigenvector)
-    arr_of_eigenvector = np.matrix(arr_of_eigenvector)
-    return arr_of_lambda, arr_of_eigenvector
 
-def timesMatrix(matrixA, matrixB) : # matrix A x matrix B
-    temp = np.dot(matrixA, matrixB)
-    temp = temp.tolist()
-    return temp
+        for i in range(len(res)) :
+            res[i] = res[i][0] / sum
 
-# Return segitiga atas dalam QR Decomposition
-def getUpperTriangle(matrix) :
-    r = np.linalg.qr(matrix, mode='r')
-    r = r.tolist()
-    lenght = len(r)
-    for row in range(lenght) :
-        for col in range(lenght) :
-            r[row][col] = round(r[row][col], 2)
-    return r
+        res = res.tolist() # isinya masih agak jelek
 
-# Return Matriks Hessenberg
-def toHessenberg(matriks) :
-    temp = np.copy(matriks)
-    temp = hessenberg(temp, overwrite_a=True)
-    return temp
+        res1= []
+        for item in res :
+            res1.append(item[0])
 
+        arr_of_res.append(res1)
+        
+    result_vector = np.matrix(arr_of_res)
+    result_vector = result_vector.T
 
-# a = np.matrix([[5,3,0],[3,5,0],[0,0,0]])
-# b, c = eigen(a)
+    for i in range(len(result)) :
+        result[i] = float(result[i].evalf()) # sekarang result dikonversi ke float
+
+    return result, result_vector
+
+# mat = np.array([[10,0,2],[0,10,4],[2,4,2]])
+# a, b = eig(mat)
+# print(a)
 # print(b)
-# print(c)
-
-# d, e = np.linalg.eig(a)
-# print(d)
-# print(e)
